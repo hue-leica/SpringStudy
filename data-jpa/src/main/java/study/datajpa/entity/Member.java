d4c8c0443ef64e3c77122ea1812626843bb1753d
jungwook <neity16@daum.net>
2021-04-08 19:55:50 +0900

    2021-04-08 스프링 데이터 JPA 시작

---
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,47 @@
+package study.datajpa.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity @Getter
+@ToString(of = {"id", "username", "age"})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private int age;
+
+    // Spring Data JPA를 사용할 때 N+1문제를 해결하기 위한 방법은 차차 설명할 예정
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    // Entity는 기본적으로 default 생성자를 가져야하며
+    // 생성을 제한하기 위해 protected로 사용하는것을 권장
+    // 이것 대신 AccessLevel.PROTECTED로 설정해도 된
+//    protected Member() {
+//    }
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        this.team = team;
+    }
+
+    /* 연관관계 메서드 */
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
---
+++ data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,31 @@
+package study.datajpa.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+
+@SpringBootTest
+@Transactional
+class MemberJpaRepositoryTest {
+
+    @Autowired MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void testMember() throws Exception{
+        //given
+        Member member = new Member("memberA");
+        Member savedMember = memberJpaRepository.save(member);
+        //when
+        Member findMember = memberJpaRepository.findOne(member.getId());
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member); // 하나의 트랜잭션 안에서는 같은 객체임을 보장함
+    }
+
+}
